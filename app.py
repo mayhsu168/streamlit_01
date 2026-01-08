76c2b256524da2337b6e699d82d2329d945edfbd
mayhsu168 <mayhsu721@gmail.com>
2026-01-08 10:52:45 +0800

    Refactor form inputs into a Streamlit form

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
 import streamlit as st
 
-st.title("資料送出清空")
+st.title("資料送出後清空資料")
 
 if "save_data" not in st.session_state:
     st.session_state.save_data=[]
 
-if "name" not in st.session_state:
-    st.session_state.name=""
-    st.session_state.grender="man"
-    st.session_state.age=0
-    st.session_state.birthday=None
-    st.session_state.fruit="apple"
-    st.session_state.message=""
-    st.session_state.aggre=False
-
-st.text_input("name",key="name")
-st.radio("grender",["man","feman"],key="grender")
-st.number_input("age",min_value=0,max_value=120,step=1,key="age")
-st.date_input("birthday",key="birthday")
-st.selectbox("fruit",["apple","banana","orange"],key="fruit")
-st.text_area("message",key="message")
-st.checkbox("agree",key="agree")
+with st.form("aform",clear_on_submit=True):
+    name=st.text_input("name")
+    grender=st.radio("grender",["man","feman"])
+    age=st.number_input("age",min_value=0,max_value=120,step=1)
+    birthday=st.date_input("birthday")
+    fruit=st.selectbox("fruit",["apple","banana","orange"])
+    agree=st.checkbox("agree",value=False)
+    message=st.text_area("message")
 
 if st.button("submit"):
     dic={
-        "name":st.session_state.name,
-        "gender":st.session_state.grender,
-        "age":st.session_state.age,
-        "birthday":st.session_state.birthday,
-        "fruit":st.session_state.fruit,
-        "message":st.session_state.message,
-        "agree":st.session_state.agree
+        "name":name,
+        "grender":grender,
+        "age":age,
+        "birthday":birthday,
+        "fruit":fruit,
+        "message":message,
+        "agree":agree
     }
 
     st.session_state.save_data.append(dic)
 
-    st.session_state.name=""
-    st.session_state.grender="man"
-    st.session_state.age=0
-    st.session_state.birthday=None
-    st.session_state.fruit="apple"
-    st.session_state.message=""
-    st.session_state.aggre=False
-
 st.write(st.session_state.save_data)
-   
